d7eace1a990e696fd91fbe2c5ecaf1f0118bb0da
alex <8379108+Alex33856@users.noreply.github.com>
2026-05-06 12:23:29 -0400

    Clean up main class

--- src/main/kotlin/com/operationpotato/itemlist/SkyBlockItemList.kt
+++ src/main/kotlin/com/operationpotato/itemlist/SkyBlockItemList.kt
@@ -6,32 +6,33 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents
 import net.fabricmc.fabric.api.client.screen.v1.ScreenKeyboardEvents
 import net.fabricmc.fabric.api.client.screen.v1.ScreenMouseEvents
 import net.fabricmc.fabric.api.client.screen.v1.Screens
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen
 import net.minecraft.client.gui.screens.inventory.InventoryScreen
-import org.slf4j.LoggerFactory
 import tech.thatgravyboat.skyblockapi.api.location.LocationAPI
 import tech.thatgravyboat.skyblockapi.utils.extentions.right
 
 object SkyBlockItemList : ClientModInitializer {
-	private val logger = LoggerFactory.getLogger("skyblock-item-list")
 
 	override fun onInitializeClient() {
-		logger.info("Hello Fabric world!")
-		ScreenEvents.AFTER_INIT.register { mc, screen, w, h ->
-			if (!LocationAPI.isOnSkyBlock) return@register
-			if (screen is AbstractContainerScreen<*>) {
-				if (screen is InventoryScreen && mc.player?.hasInfiniteMaterials() ?: false) return@register
-				val width = w - screen.right
-				val itemList = EntireListWidget(screen.right, 0, width, h)
+		ScreenEvents.AFTER_INIT.register(::addItemListWidget)
+	}
 
-				Screens.getWidgets(screen).add(itemList)
-				ScreenMouseEvents.allowMouseScroll(screen).register { _, x, y, scrollX, scrollY ->
-					itemList.mouseScrolled(x, y, scrollX, scrollY)
-				}
-				ScreenKeyboardEvents.allowKeyPress(screen).register { _, event ->
-					itemList.keyPressed(event)
-					return@register true
-				}
+	fun addItemListWidget(mc: Minecraft, screen: Screen, w: Int, h: Int) {
+		if (!LocationAPI.isOnSkyBlock) return
+		if (screen is AbstractContainerScreen<*>) {
+			if (screen is InventoryScreen && mc.player?.hasInfiniteMaterials() ?: false) return
+			val width = w - screen.right
+			val itemList = EntireListWidget(screen.right, 0, width, h)
+
+			Screens.getWidgets(screen).add(itemList)
+			ScreenMouseEvents.allowMouseScroll(screen).register { _, x, y, scrollX, scrollY ->
+				itemList.mouseScrolled(x, y, scrollX, scrollY)
+			}
+			ScreenKeyboardEvents.allowKeyPress(screen).register { _, event ->
+				itemList.keyPressed(event)
+				return@register true
 			}
 		}
 	}
