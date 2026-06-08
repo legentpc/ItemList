a35eaabf15a66d6140af6488b73b9a041c98051f
alex <8379108+Alex33856@users.noreply.github.com>
2026-06-08 16:01:52 -0400

    feat: add npc location to lore

--- src/main/kotlin/com/operationpotato/itemlist/utils/SkyBlockMobsRepo.kt
+++ src/main/kotlin/com/operationpotato/itemlist/utils/SkyBlockMobsRepo.kt
@@ -3,12 +3,18 @@ package com.operationpotato.itemlist.utils
 import com.mojang.authlib.properties.Property
 import net.minecraft.core.component.DataComponents
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
 import net.minecraft.world.item.Items
+import net.minecraft.world.item.component.ItemLore
 import tech.thatgravyboat.repolib.api.RepoAPI
+import tech.thatgravyboat.repolib.api.mobs.Mob
+import tech.thatgravyboat.skyblockapi.api.location.SkyBlockIsland
 import tech.thatgravyboat.skyblockapi.api.repo.LazyItemStack
 import tech.thatgravyboat.skyblockapi.api.repo.apis.RepoItemCache
 import tech.thatgravyboat.skyblockapi.platform.ResolvableProfile
 import tech.thatgravyboat.skyblockapi.utils.extentions.toTitleCase
+import tech.thatgravyboat.skyblockapi.utils.text.Text
+import tech.thatgravyboat.skyblockapi.utils.text.TextColor
 import tech.thatgravyboat.skyblockapi.utils.text.TextStyle.style
 
 object SkyBlockMobsRepo : RepoItemCache<String>("Mobs") {
@@ -27,13 +33,34 @@ object SkyBlockMobsRepo : RepoItemCache<String>("Mobs") {
 
 		val stackName = Component.literal("${data.name} ($suffix)")
 			.style { withItalic(false) }
+		val lore = createLore(data)
+
 		// TODO: replace barrier with mob item when it's available
 		val stack = LazyItemStack(Items.BARRIER.takeIf { data.texture == null } ?: Items.PLAYER_HEAD) {
 			if (data.texture != null) {
 				this[DataComponents.PROFILE] = ResolvableProfile { put("textures", Property("textures", data.texture)) }
 			}
 			this[DataComponents.CUSTOM_NAME] = stackName
+			if (lore.isNotEmpty()) {
+				this[DataComponents.LORE] = ItemLore(lore)
+			}
 		}
 		return stack
+	}
+
+	private fun createLore(mob: Mob): List<Component> {
+		val island = SkyBlockIsland.getById(mob.island ?: "")?.displayName ?: return listOf()
+		val pos = mob.position
+
+		val style = Style.EMPTY.withItalic(false).withColor(TextColor.GRAY)
+		val lineEnding = if (pos == null) "." else ""
+
+		return listOfNotNull(
+			Text.of("Located in ").append(Text.of(island).withColor(TextColor.GOLD)).append(lineEnding)
+				.withStyle(style),
+			if (pos == null) null else
+				Text.of("at ").append(Text.of("${pos.x}, ${pos.y}, ${pos.z}").withColor(TextColor.WHITE)).append(".")
+					.withStyle(style)
+		)
 	}
 }
